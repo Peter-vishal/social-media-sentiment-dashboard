01e82995f97c86e3fd42186d213f2075679b626f
Peter Vishal <peterrintu99@gmail.com>
2025-03-15 16:56:31 +0530

    Initial commit for Streamlit app

---
+++ tw_sentiment_js/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;  
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #ccac0b;
+}
+
+#nav a.router-link-exact-active {
+  color: #678a06;
+}
+</style>
+<script>
+import NavBar from './components/NavBar.vue'
+export default {
+  name: 'Home',
+  components: {
+    NavBar
+  }
+}
+</script>
---
+++ tw_sentiment_js/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home mt-5 ml-5 mr-5">
+    <b-jumbotron header="Tweet Sentiment Dashboard" lead="Sentiment Analysis Web Application With Bootstrap4, VueJS and Python Flask">
+      <p>This app help you to analyze sentiment of a hashtag</p>
+      <div class="mb-3"><b>Follow Me</b></div>
+      <a class="btn btn-info mr-2" href="#"> On Medium</a>
+      <a class="btn btn-primary" href="#">On Facebook</a> 
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {  
+  name: 'Home',
+}
+</script>
